eb2b87e57994d9eef7f5eb37314b5f27fe8d16dc
Alejandro Roca <alekss.ro@gmail.com>
2019-04-05 12:50:17 +0200

    add comments

--- scripts/downloadData.sh
+++ scripts/downloadData.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# Script to download the datasets defined in the data/DatasetInfoFile.tsv file
 
-# 0.1 define root directorties
+# 1 define root directorties
 PROJECT_DIR=`pwd`   # should be run from project folder
 DATA_DIR=${PROJECT_DIR}/data;
 
-# 0.3 Sample-datasheet
+# 2 Sample-datasheet path
 DATASHEET_SAMPLES_FILE=${DATA_DIR}/DatasetInfoFile.tsv;
 
+# Every line contains:
+# CELL_LINE MOD_TYPE    SIGNAL_TRACK    SAMPLE_ID   FILENAME    raw REPLICATE_NUM   LAB DOWNLOAD_LINK
 while read LINE;do
 
     CELL_LINE=$(echo ${LINE} | awk '{split($0,a," ");print a[1]}');
@@ -15,9 +18,9 @@ while read LINE;do
     DOWNLOAD_LINK=$(echo ${LINE} | awk '{split($0,a," ");print a[9]}');
     echo $CELL_LINE $SIGNAL_TRACK $SAMPLE_ID $DOWNLOAD_LINK
 
+    # Create directory for each file (this file tree is needed for later steps) and download
     mkdir -p ${DATA_DIR}/${CELL_LINE}/${SIGNAL_TRACK}/${SAMPLE_ID}
 
     wget -P ${DATA_DIR}/${CELL_LINE}/${SIGNAL_TRACK}/${SAMPLE_ID} $DOWNLOAD_LINK
-
 
 done < ${DATASHEET_SAMPLES_FILE};
