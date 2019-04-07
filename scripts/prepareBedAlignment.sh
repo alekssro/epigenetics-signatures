2305da7a1bb227272b4f1d2037ac70a8251c2ea8
Alejandro Roca <alekss.ro@gmail.com>
2019-04-07 13:23:56 +0200

    Remove unnecesary code and extend explanation

--- scripts/prepareBedAlignment.sh
+++ scripts/prepareBedAlignment.sh
@@ -31,15 +31,16 @@ DATASHEET_SAMPLES_FILE=${DATA_DIR}/DatasetInfoFile.tsv;		# file containing info 
 cd ${DATA_DIR} || exit;		# change to data directory or exit
 
 
-# 1.1 read the datasheet-tab-delimited file line by line
-while read LINE; do
+# 1 read the datasheet-tab-delimited file line by line
+while read LINE; do			# Read ${DATASHEET_SAMPLES_FILE}
 
 	# get values in single fields;
 	CELL_LINE=$(echo ${LINE} | awk '{split($0,a," ");print a[1]}');
+	ASSAY_TYPE=$(echo ${LINE} | awk '{split($0,a," ");print a[2]}');
 	ASSAY=$(echo ${LINE} | awk '{split($0,a," ");print a[3]}');
 	SAMPLE_ID=$(echo ${LINE} | awk '{split($0,a," ");print a[4]}');
 	ALIGN_FILENAME=$(echo ${LINE} | awk '{split($0,a," ");print a[5]}');
-	ALIGN_FILETYPE=$(echo ${LINE} | awk '{split($0,a," ");print a[6]}');
+	# ALIGN_FILETYPE=$(echo ${LINE} | awk '{split($0,a," ");print a[6]}');
 
 	# 1.2 check if the alignment file is available or not
 	if [ -z "$ALIGN_FILENAME" ]
@@ -47,111 +48,64 @@ while read LINE; do
 	    continue
 	fi
 
-
 	#progress
-	echo "working on sample ${SAMPLE_ID} assay ${ASSAY} cell-line ${CELL_LINE}";
-
-	# exit; 		#debug
-
-	# 1.3 check if processed.bed file is already provided and in this case re-format this.
-	if [ $ALIGN_FILETYPE = "processed" ]
-	then
-		echo "processed bed file $ALIGN_FILENAME";
-
-		# 1.4 Replace empty-space SF by tabs
-		echo "replace empty spaces with tabs..";
-		sed 's/ /\t/g' ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/${ALIGN_FILENAME} > ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed
-
-		echo "verify columns in the bed format..";
-		NCOL=$(awk -F '\t' '{print NF;exit}' ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed)
-		if [ "$NCOL" -eq 5 ]
-		then
-			echo "adjust number of columns";
-			sed -i "s/$/\t255/" ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed;
-			awk -F "\t" '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $6 "\t" $5}' ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed > ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp1.bed;
-		else
-			echo "processed bed file $ALIGN_FILENAME is correctly formatted";
-			cp ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp1.bed
-		fi
-
-		# 1.5 Add 'processed' suffix to the processed .bed file
-		PROCESSED_FILENAME=${ALIGN_FILENAME/".bed"/".processed.bed"};
-		# Remove anomalous position
-		echo "remove anomalous position along the chromosomes..";
-		awk -F "\t" '$2 >= 0 {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp1.bed > ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/${PROCESSED_FILENAME};
-		# Cancel temporary files
-		rm ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp0.bed;rm ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE_ID}/temp1.bed;
-		echo "alignment file is ${ALIGN_FILETYPE} and skip";
-		continue
-	fi  # close block for 'processed bed files'
-
+	echo "Working on sample ${SAMPLE_ID} assay ${ASSAY} cell-line ${CELL_LINE}"
 
 	# @bam files or .bed not-processed@:
-	echo "raw bed OR bam file $ALIGN_FILENAME";
+	echo "\traw bam file $ALIGN_FILENAME";
 	CELL_LINE_DIR=${DATA_DIR}/${CELL_LINE};
 	SAMPLE_DIR=${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID};
 	# get the path to the alignment file
 	ALIGN_FILEPATH=${SAMPLE_DIR}/${ALIGN_FILENAME};
 
-
 	# take note of every bam file contained;
 	cd ${SAMPLE_DIR} || exit;
-	#echo "DIRECTORY is ${DATA_DIR}/${CELL_LINE}/${ASSAY}/${SAMPLE}";
 
-	# If there is a .bed file (this is in raw format), convert it into .bam
+	# If there is a .bed file, convert it into .bam
 	bedcount=`ls -1 *.bed 2>/dev/null | wc -l`
 	if [ "$bedcount" -gt 1 ]
 	then
-	    echo "convert bed to bam..";
-	    # define the output file name just generated
+	    echo "Converting bed to bam..";
+	    # define the output file name just generated (same name, change extension .bed to .bam )
 	    BAM_FILE=${ALIGN_FILENAME/".bed"/".bam"};
 	    bedtools bedtobam -i ${ALIGN_FILEPATH} -g ${DATA_DIR}/chromInfo.txt > ${BAM_FILE};
 	else
 	    BAM_FILE=${ALIGN_FILENAME};
 	fi
 
-
-	#NB: Starting from here we always have a .bam file!
-
 	# Retrieve only the sample name:
 	BAM_NAME=${BAM_FILE/".bam"/""};
 
 	# sort and index bam file;
-	echo "sort and index ${BAM_NAME}";
+	echo "Sort and index ${BAM_NAME}";
     samtools sort ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_FILE} -o ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.sorted.bam
     samtools index ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.sorted.bam
 
-	# remove duplicate reads;
-	echo "mark and remove duplicates in ${BAM_NAME}";
+	# remove duplicate reads
+	echo "Mark and remove duplicates in ${BAM_NAME}";
 	samtools rmdup ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.sorted.bam ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.nodup.bam
-    # PicardCommandLine MarkDuplicates INPUT=${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.sorted.bam OUTPUT=${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.nodup.bam ASSUME_SORTED=True REMOVE_DUPLICATES=True METRICS_FILE=${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.metrics.log VALIDATION_STRINGENCY=SILENT
-
-	# Skip "remove duplicate reads"
-	# mv ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.sorted.bam ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.nodup
-	# ls -l ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}
 
     # take only reliable alignments ( only matches with 99% probability of being real matches are kept )
-	echo "make a new bam-index and keep only high-quality alignments in ${BAM_NAME}";
+	echo "Make a new bam-index and keep only high-quality alignments in ${BAM_NAME}";
 	samtools index ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.nodup.bam
 	samtools view -q 20 -b ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.nodup.bam > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.filtered.bam
 	# Alternatively, if Bowtie was used, you select uniquematch reads:
 	# /usr/bin/samtools view -f 0x2 -q 255 -b tumor1.bam -o unique.bam
 
 	# convert .bam to .bed (bedtools bamtobed);
-	echo "convert ${BAM_NAME} into .bed format";
-
+	echo "Convert ${BAM_NAME} into .bed format";
 	bamToBed -i ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.filtered.bam  > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed
 
 	# For TFs and HistMod signals apply tag extension of 200bp
-	## split by strand +/- ;
-	### apply tag extension of 200bp;
-	#### merge extended bed files
+	# 	- split by strand +/- ;
+	# 	- apply tag extension of 200bp;
+	# 	- merge extended bed files
 
 	cd ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID} || exit;
 
-	if [ "$ASSAY" != "DNase-seq" ]
+	if [ "$ASSAY_TYPE" != "openchromatin" ]
 	then
-	    echo "apply tag extension on ${BAM_NAME}";
+	    echo "Apply tag extension on ${BAM_NAME} and saving into a processed bed file";
 	    awk -F "\t" '$6=="+" {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.pos.bed
 	    awk -F "\t" '$6=="-" {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.neg.bed
 	    awk -F "\t" '{print $1,$2,$2+199,$4,$5,$6}' ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.pos.bed > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.extpos.bed
@@ -160,9 +114,10 @@ while read LINE; do
 
 	    # remove intermediate files
 	    rm ${BAM_NAME}.ext*; rm ${BAM_NAME}.pos.bed;rm ${BAM_NAME}.neg.bed;
-	    # otherwise sort the .bed file only
+
 	else
-	    echo "sort by strand,chromosome,pos $BAM_NAME";
+		# otherwise sort the .bed file only
+	    echo "Sort by strand,chromosome,pos $BAM_NAME";
 	    sort -k6,6 -k1,1 -k2,2n ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed -o ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed
 	fi
 
@@ -171,7 +126,6 @@ while read LINE; do
 
 	# Remove anomalous position
 	awk -F "\t" '$2 >= 0 {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/out.bed > ${CELL_LINE_DIR}/${ASSAY}/${SAMPLE_ID}/${BAM_NAME}.processed.bed
-
 
 	# remove intermediate files
 	rm ${BAM_NAME}.nodup*; rm ${BAM_NAME}.sorted*; rm ${BAM_NAME}.filtered.bam; rm out.bed;
