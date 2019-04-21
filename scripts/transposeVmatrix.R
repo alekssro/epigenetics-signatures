dd880c7ecb91b882a1ba6df13ff1fc7c62b2a9dd
Alejandro Roca <alekss.ro@gmail.com>
2019-04-21 16:03:53 +0200

    Take input and output path from arguments

---
+++ scripts/transposeVmatrix.R
@@ -0,0 +1,19 @@
+require(data.table)
+
+# Get arguments from terminal call
+args <- commandArgs(trailingOnly = T)       # trailingOnly = T, gets only arguments not the call
+
+V <- fread(args[1], sep = ",", header = F)
+
+if (nrow(V) < ncol(V)) {
+    V_mat <- t(V)
+    
+    V_matrix <- matrix(as.numeric(V_mat[-1, ]), nrow = nrow(V_mat) - 1, ncol = ncol(V_mat))
+    
+    colnames(V_matrix) <- V_mat[1, ]
+    
+    write.csv(V_matrix, file = args[2], sep = ",", quote = F, row.names = F)
+}
+
+
+
