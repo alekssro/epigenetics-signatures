2e27849c67e28eeb84906211d048a07d200311a8
Alejandro Roca <alekss.ro@gmail.com>
2019-05-07 11:49:00 +0200

    create separate file for NMF analysis

--- scripts/NMFanalysis.R
+++ scripts/NMFanalysis.R
@@ -1,63 +1,12 @@
-# #Resampling 1 set of counts using a multinomial distribution (here 3 categories)
+#!/usr/bin/Rscript
+
+###################################################################################################
+# Script used to perform NMF analysis in the filtered V matrix data                             ###
 # 
-# ncounts <- c(100, 230, 45)
-# 
-# tot <- sum(ncounts)
-# 
-# vecp <- ncounts/tot
-# 
-# rmultinom(n = 1,size = tot,prob = vecp)
+###################################################################################################
+# Done by Alejandro Roca (alekss.ro@gmail.com)                                                  ###
+###################################################################################################
 
-library(tidyverse)
-library(reshape2)
-
-setwd("/mnt/DataHDD/alekssro/Nextcloud/Universidad/Bioinformatics/MasterThesis/mscthesis")
-
-V <- read.csv("results/genomic_survey/HepG2/V_matrix_transposed.csv", header = T)
-chrmsL <- read.table("data/chromInfo.txt")
-
-# assign chrm names and rebuild bin names
-chrmsNames <- rep("", times=length(V[,1]))
-binNames <- rep(0, times=length(V[,1]))
-start <- 1
-for (i in 1:length(chrmsL[,1])) {
-    
-    nBins <- ceiling(chrmsL[i, 2]/200.0)
-
-    chr <- rep(as.character(chrmsL[i, 1]), nBins)
-    bin <- as.numeric(seq(from = 1, to = chrmsL[i, 2], by=200))
-    
-    end <- start + nBins - 1
-    # print(c(end - start, nBins, length(chr), length(bin)))
-    chrmsNames[start:end] <- chr
-    binNames[start:end] <- bin
-    start <- end + 1
-}
-
-V$chr <- chrmsNames
-V$binStart <- binNames
-
-row_means <- rowMeans(V[,1:8])
-
-noHits_i <- row_means < max(row_means) * 0.001
-sum(noHits_i)
-
-filteredV <- V[!noHits_i,]
-dim(filteredV)
-
-chrs <- unique(V$chr)
-
-for (i in 1:length(chrs)) {
-    d <- filteredV %>% filter(chr == chrs[i])
-    d <- melt(d, id.vars = "binStart")
-    
-    p <- d %>% filter(variable != "chr") %>%
-        ggplot(aes(x=binStart, y=as.numeric(value), col=variable)) + 
-        geom_point(aes(color=factor(variable)))
-
-    ggsave(filename = paste("plots/", chrs[i], "_readsDistr.png", sep=""), plot = p)
-        
-}
 
 source(file = "scripts/more/MajorizeMinimizeNQP.R")
 
@@ -129,4 +78,3 @@ ggplot(NULL) +
     theme_minimal() +
     xlab("Patient") + ylab('Mutation Load') +
     ggtitle('Mutation Load for all patients towards each signature')
-
