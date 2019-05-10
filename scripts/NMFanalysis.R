7717af3a40a6e772f248dde7f8d674faca268d15
Alejandro Roca <alekss.ro@gmail.com>
2019-05-10 12:59:15 +0200

    Use NMF package (faster)

--- scripts/NMFanalysis.R
+++ scripts/NMFanalysis.R
@@ -17,12 +17,18 @@
 # 
 # rmultinom(n = 1,size = tot,prob = vecp)
 
+suppressPackageStartupMessages(require(tidyverse, quietly = T, warn.conflicts = F))
+suppressPackageStartupMessages(require(NMF, quietly = T, warn.conflicts = F))
 
 source(file = "scripts/more/MajorizeMinimizeNQP.R")
 
+# Load V matrix and chromosome info
+cat("Loading V matrix...\n")
+filteredV <- read.csv("results/genomic_survey/HepG2/filteredV.csv", header = T)
+
 epimarks <- colnames(filteredV[,1:8])
-chr1V <- filteredV %>% filter(chr == "chr1")
-V <- as.matrix(chr1V[,1:8])
+# chr1V <- filteredV %>% filter(chr == "chr1")
+V <- as.matrix(filteredV[,1:8])
 n_bins <- nrow(V)
 n_epimarks <- ncol(V)
 nIter <- 30   # From a run of 100 iters, RSS doesn't change after 25-30 iterations
@@ -39,42 +45,48 @@ MM.RSS.n <- matrix(data = 0, nrow = nIter, ncol = 8)
 for (n in 1:8) {
     
     n_signatures <- n
-    # Initialize W and load H
-    W <- matrix(runif(n_bins*n_signatures, 0, 1), nrow = n_bins, ncol = n_signatures)
-    H <- matrix(runif(n_signatures*n_epimarks, 0, 1), nrow = n_signatures, ncol = n_epimarks)
     
-    for (iter in 1:nIter) {
-        
-        # Update rows in W; every patient
-        for (i in 1:n_bins) {  # nrow(W)
-            v <- V[i, ]     # we use rows of V
-            
-            # For A and b, opposite dimensions as in column update in H
-            A = 2 * H %*% t(H)     
-            b = as.vector(-2 * H %*% v)
-            
-            MMres <- MajorizeMinimizeNQP(A = A, b = b, init = W[i,], maxIter = 10, tol = 1e-10)
-            
-            # Replace old by updated row
-            W[i, ] <- MMres$x
-        }
-        
-        # Calculate RSS for each iteration of the algorithm
-        MM.RSS[iter] <- sum((V - W %*% H) ^ 2)
-        
-        MM.RSS.n[iter, n] <- MM.RSS[iter]
-        
-        # # Keep track of the convergence condition evolution
-        # if (iter %% 10 == 0) {cat("Iteration ", iter, ", log RSS decrease: ", 
-        #                           log(MM.RSS[iter - 1]) - log(MM.RSS[iter]), "\n", 
-        #             sep = '')}
-        
-        # Convergence conditioned on log(RSS) increase
-        if ( (iter > 1) && ((log(MM.RSS[iter - 1]) - log(MM.RSS[iter])) < s) ) {break}
-        
-    }
+    nmf_res <- nmf(V, n)
     
-    print(paste("N:", n, "sum(V - W * H)^2 =", MM.RSS[iter]))
+    ############# CONTINUE HERE #################
+    
+    # # Initialize W and load H
+    # W <- matrix(runif(n_bins*n_signatures, 0, 1), nrow = n_bins, ncol = n_signatures)
+    # H <- matrix(runif(n_signatures*n_epimarks, 0, 1), nrow = n_signatures, ncol = n_epimarks)
+    # 
+    # for (iter in 1:nIter) {
+    #     
+    #     # Update rows in W; every patient
+    #     for (i in 1:n_bins) {  # nrow(W)
+    #         v <- V[i, ]     # we use rows of V
+    #         
+    #         # For A and b, opposite dimensions as in column update in H
+    #         A = 2 * H %*% t(H)     
+    #         b = as.vector(-2 * H %*% v)
+    #         
+    #         MMres <- MajorizeMinimizeNQP(A = A, b = b, init = W[i,], maxIter = 10, tol = 1e-10)
+    #         
+    #         # Replace old by updated row
+    #         W[i, ] <- MMres$x
+    #     }
+    #     
+    #     # Calculate RSS for each iteration of the algorithm
+    #     MM.RSS[iter] <- sum((V - W %*% H) ^ 2)
+    #     
+    #     MM.RSS.n[iter, n] <- MM.RSS[iter]
+    #     
+    #     # # Keep track of the convergence condition evolution
+    #     # if (iter %% 10 == 0) {cat("Iteration ", iter, ", log RSS decrease: ", 
+    #     #                           log(MM.RSS[iter - 1]) - log(MM.RSS[iter]), "\n", 
+    #     #             sep = '')}
+    #     
+    #     # Convergence conditioned on log(RSS) increase
+    #     if ( (iter > 1) && ((log(MM.RSS[iter - 1]) - log(MM.RSS[iter])) < s) ) {break}
+    #     
+    # }
+    
+    print(paste("N:", n, "sum(V - W * H)^2 =", nmf_res@residuals))
+    
 }
 
 iters <- apply(MM.RSS.n, 2, function(x) sum(x > 0))
